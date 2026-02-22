57b841b07727408863baf14e6843be3114d38c50
tejasderle24 <tejasderle24@gmail.com>
2026-02-22 11:16:03 +0530

    chanegs

--- src/Component/API/Url.jsx
+++ src/Component/API/Url.jsx
@@ -1,4 +1,4 @@
-// export const BASEURL = "https://dashboard.theceramicstudio.in/";
+export const BASEURL = "https://dashboard.theceramicstudio.in";
 
 //  export const BASEURL = "http://localhost:5000";
-export const BASEURL = "https://dashboarduat.theceramicstudio.in";
+// export const BASEURL = "https://dashboarduat.theceramicstudio.in";
